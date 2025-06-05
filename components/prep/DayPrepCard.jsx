d75aef42344d9073a84d634cd3530d3504b74835
DTmopped <dt@moppedbbq.com>
2025-06-05 19:46:36 -0400

    Update DayPrepCard.jsx

--- components/prep/DayPrepCard.jsx
+++ components/prep/DayPrepCard.jsx
@@ -5,7 +5,7 @@ import { Checkbox } from '@/components/ui/checkbox';
 import { Label } from '@/components/ui/label';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { motion } from 'framer-motion';
-import { cn } from "@/lib/utils";
+import { cn } from '../../src/lib/utils';
 
 const DayPrepCard = ({ dayData, onPrepTaskChange }) => {
   if (!dayData) return null;
