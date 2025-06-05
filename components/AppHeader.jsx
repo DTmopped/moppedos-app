4126a54db5f0f6430e0b34ab5f446da617b24383
DTmopped <dt@moppedbbq.com>
2025-06-05 19:56:54 -0400

    Update AppHeader.jsx

--- components/AppHeader.jsx
+++ components/AppHeader.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { Zap } from 'lucide-react';
-import { cn } from '../src/lib/utils';
+import { cn } from '../lib/utils';
 import { Button } from './ui/button';
 const AppHeader = ({ views }) => {
   const navigationViews = views ? views.filter(view => view.showInNav) : [];
