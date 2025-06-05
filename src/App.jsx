a1f645ec1fab76e7ca4943e6a0cec2704bbea929
DTmopped <dt@moppedbbq.com>
2025-06-05 13:59:00 -0400

    Fix App.jsx to use AppRouter and AppProviders

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
-import { useState } from 'react'
+import React from 'react';
+import AppRouter from './AppRouter';
+import AppProviders from './AppProviders';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hostinger Horizons</h1>
-        <p>Your React application is now working!</p>
-        <button onClick={() => setCount((count) => count + 1)}>
-          Count is: {count}
-        </button>
-      </header>
-    </div>
-  )
+    <AppProviders>
+      <AppRouter />
+    </AppProviders>
+  );
 }
 
-export default App
+export default App;
