119b7ee5b6ffa548795f360bd10d6c01e8246dea
DTmopped <dt@moppedbbq.com>
2025-06-04 20:54:09 -0400

    Clean vite.config.js to remove broken aliases

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      './App.jsx': path.resolve(__dirname, './src/src/src/App.jsx'),
-      './index.css': path.resolve(__dirname, './src/src/src/index.css')
-    }
-  }
-})
+});
