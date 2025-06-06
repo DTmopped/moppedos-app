9b25474e496c92ff385ca052abb229c6f3e6ceb0
DTmopped <dt@moppedbbq.com>
2025-06-06 17:02:06 -0400

    Update EmptyState.jsx

--- components/EmptyState.jsx
+++ components/EmptyState.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-import { Button } from "@/components/ui/button";
+import { Button } from "components/ui/button.jsx";
 import { ClipboardList } from "lucide-react";
 
 const EmptyState = ({ onAddTask }) => {
