0b01e71aa74d81fe7aa4f0de3d5d9200d6b0dc28
DTmopped <dt@moppedbbq.com>
2025-06-06 17:30:53 -0400

    Update SimpleSupabaseTest.jsx

--- components/SimpleSupabaseTest.jsx
+++ components/SimpleSupabaseTest.jsx
@@ -1,7 +1,7 @@
 
 import React, { useState } from 'react';
-import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Button } from 'components/ui/button.jsx';
+import { Card, CardContent, CardHeader, CardTitle } from 'components/ui/card.jsx';
 
 const SimpleSupabaseTest = () => {
   const [result, setResult] = useState('');
