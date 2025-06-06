a25bdd29323e80a8acd016ca75c3a0b4caf377f6
DTmopped <dt@moppedbbq.com>
2025-06-05 20:03:08 -0400

    Update alert-dialog.jsx

--- components/ui/alert-dialog.jsx
+++ components/ui/alert-dialog.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import * as AlertDialogPrimitive from "@radix-ui/react-alert-dialog";
-import { cn } from '../../src/lib/utils';
+import { cn } from '../../lib/utils';
 import { buttonVariants } from "@/components/ui/button";
 
 const AlertDialog = AlertDialogPrimitive.Root;
