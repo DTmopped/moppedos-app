c82bcc09a557797be8e68736167ea858d1ebce76
DTmopped <dt@moppedbbq.com>
2025-06-05 20:14:15 -0400

    Update OrderGuideCategory.jsx

--- components/orderguide/OrderGuideCategory.jsx
+++ components/orderguide/OrderGuideCategory.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
-import { cn } from '../../src/lib/utils'; For conditional class names
+import { cn } from '../../lib/utils'; For conditional class names
 
 const OrderGuideCategoryComponent = ({ categoryTitle, items, getStatusClass, getStatusIcon, icon: Icon }) => {
   if (!items || items.length === 0) {
