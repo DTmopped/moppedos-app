09864edb082b2e45f38431739be9cc31b5eedf97
DTmopped <dt@moppedbbq.com>
2025-06-07 12:23:49 -0400

    Update toaster.jsx

--- components/ui/toaster.jsx
+++ components/ui/toaster.jsx
@@ -1,5 +1,5 @@
 import { Toast, ToastClose, ToastDescription, ToastProvider, ToastTitle, ToastViewport } from './toast';
-import { useToast } from './use-toast';
+import { useToast } from './use-toast.jsx';
 import React from 'react';
 
 export function Toaster() {
