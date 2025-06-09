c36cb752133b4cf2a7c494ee53c4fd126c0a88e7
DTmopped <dt@moppedbbq.com>
2025-06-09 10:03:35 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { HashRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { HashRouter } from "react-router-dom";
+import { DataProvider } from "./contexts/DataContext";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <DataProvider>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </DataProvider>
   </React.StrictMode>
-)
+);
