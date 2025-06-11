478f5ca599dfa74840a4d7ef8e5c33fe38ef7271
DTmopped <dt@moppedbbq.com>
2025-06-11 11:24:12 -0400

    Update DailyShiftPrepGuideHeader.jsx

--- src/components/prep/DailyShiftPrepGuideHeader.jsx
+++ src/components/prep/DailyShiftPrepGuideHeader.jsx
@@ -86,14 +86,16 @@ const DailyShiftPrepGuideHeader = ({
                     </p>
                   )}
                 </div>
-                <DialogFooter className="sm:justify-between">
-                  <DialogClose asChild>
-                    <Button variant="outline">Cancel</Button>
-                  </DialogClose>
-                  <Button onClick={handleSaveClicked} className="btn-gradient">
-                    <Save className="mr-2 h-4 w-4" /> Save Menu
-                  </Button>
-                </DialogFooter>
+                <DialogFooter className="flex justify-end gap-3 mt-2 sm:mt-4 border-t border-slate-700 pt-4">
+  <DialogClose asChild>
+    <Button variant="outline" className="text-sm px-4 py-2">
+      Cancel
+    </Button>
+  </DialogClose>
+  <Button onClick={handleSaveClicked} variant="gradient" className="px-4 py-2 text-sm font-semibold">
+    <Save className="mr-2 h-4 w-4" /> Save Menu
+  </Button>
+</DialogFooter>
               </DialogContent>
             </Dialog>
 
