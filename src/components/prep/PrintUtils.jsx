a84590f990391f35009adbfb714d3954ad6824eb
DTmopped <dt@moppedbbq.com>
2025-06-16 12:13:19 -0400

    Update PrintUtils.jsx

--- src/components/prep/PrintUtils.jsx
+++ src/components/prep/PrintUtils.jsx
@@ -1,15 +1,16 @@
-import ReactDOMServer from 'react-dom/server';
+import React from "react";
+import ReactDOMServer from "react-dom/server";
 
-export const triggerPrint = (Component, props, title) => {
-  const html = ReactDOMServer.renderToStaticMarkup(<Component {...props} />);
+export const triggerPrint = (ComponentFn, props = {}, title = "Print") => {
+  const html = ReactDOMServer.renderToStaticMarkup(ComponentFn(props));
 
-  const iframe = document.createElement('iframe');
-  iframe.style.position = 'fixed';
-  iframe.style.right = '0';
-  iframe.style.bottom = '0';
-  iframe.style.width = '0';
-  iframe.style.height = '0';
-  iframe.style.border = '0';
+  const iframe = document.createElement("iframe");
+  iframe.style.position = "fixed";
+  iframe.style.right = "0";
+  iframe.style.bottom = "0";
+  iframe.style.width = "0";
+  iframe.style.height = "0";
+  iframe.style.border = "0";
   document.body.appendChild(iframe);
 
   const doc = iframe.contentWindow.document;
