7b615a56d5ec540ee9d957ee626c6ecd7f42238d
DTmopped <dt@moppedbbq.com>
2025-09-19 13:34:23 -0400

    Update FvaDashboard.jsx

--- src/components/FvaDashboard.jsx
+++ src/components/FvaDashboard.jsx
@@ -282,6 +282,13 @@ const exportToCSV = () => {
   link.click();
   URL.revokeObjectURL(url);
 };
+  return (
+  <motion.div
+    initial={{ opacity: 0, y: 20 }}
+    animate={{ opacity: 1, y: 0 }}
+    transition={{ duration: 0.5 }}
+    className="space-y-6"
+  >
 
     {/* ✅ Render Last Month Cards if toggled */}
     {showLastMonth && renderLastMonthCards()}
@@ -509,6 +516,7 @@ const exportToCSV = () => {
           </p>
         </CardContent>
       </Card>
+  </motion.div>
   );
 };
 
