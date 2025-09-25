d8cf65e2393c836f785b196e0669b27dda8c664f
DTmopped <dt@moppedbbq.com>
2025-09-24 23:38:27 -0400

    Update DailyBriefingBuilder.jsx

--- src/components/DailyBriefingBuilder.jsx
+++ src/components/DailyBriefingBuilder.jsx
@@ -370,7 +370,7 @@ const DailyBriefingBuilder = () => {
               type="date"
               value={date}
               onChange={(e) => setDate(e.target.value)}
-              className="w-auto px-4 py-2 h-10 flex items-center"
+              className="w-auto px-4 py-2 h-10"
             />
           </div>
           
