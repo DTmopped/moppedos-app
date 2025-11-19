42748dcf3672f9f0d2d31469de82e40fd11503b3
DTmopped <dt@moppedbbq.com>
2025-11-18 23:03:54 -0500

    Update AppRouter.jsx

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -2,18 +2,25 @@ import React from 'react';
 import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import AppLayout from './AppLayout'; 
 import { views } from '@/config/views';
+import TenantSettingsTest from './components/settings/TenantSettingsTest';
 
 const AppRouterComponent = () => {
   const defaultViewPath = views.find(v => v.isDefault)?.path || views[0]?.path || "/";
-
+  
   return (
     <Router>
       <Routes>
         <Route path="/" element={<AppLayout />}>
           <Route index element={<Navigate to={defaultViewPath} replace />} />
+          
           {views.map((view) => (
             <Route key={view.id} path={view.path} element={null} />
           ))}
+          
+          {/* ADD YOUR ROUTE HERE - BEFORE THE WILDCARD */}
+          <Route path="/test-settings" element={<TenantSettingsTest />} />
+          
+          {/* WILDCARD MUST BE LAST */}
           <Route path="*" element={<Navigate to={defaultViewPath} replace />} />
         </Route>
       </Routes>
